37625b532b86f525031abef0a425590794dfd349
carnival <18852862960@163.com>
2020-12-10 10:41:59 +0800

    first submit

---
+++ src/script/list_module.js
@@ -0,0 +1,33 @@
+define(['jlazyload'], () => {
+    return {
+        init: function() {
+            const $list = $('.list ul');
+            //1.渲染list.html页面
+            $.ajax({
+                url: 'http://localhost/dashboard/JS2010/week06/Day%2029-Day%2031_jquery/mine/projectname/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+                            <a href="detail.html?sid=${value.sid}">
+                                <img src="${value.url}"/>
+                                <p>${value.title}</p>
+                                <span>￥${value.price}</span>
+                            </a>
+                        </li>
+                    `;
+                });
+                $list.html($strhtml);
+                //渲染的下面进行懒加载操作
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：谈入
+                    });
+                });
+            });
+        }
+    }
+
+});
